b25dabd525537ddf6e42e2e438e74e98c00637fc
hvtnguyenson-code <hvtnguyenson@gmail.com>
2026-08-23 22:39:41 +0700

    feat(api): add internal personal reporting projection

---
+++ apps/api/src/personal-reporting-projection/personal-reporting-projection.policy.ts
@@ -0,0 +1,4 @@
+import { Injectable } from '@nestjs/common';
+import { PersonalReportingClock } from './personal-reporting-projection.types';
+@Injectable()
+export class SystemPersonalReportingClock implements PersonalReportingClock { now(): Date { return new Date(); } }
