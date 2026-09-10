c1a9fc3e6a1ba7a94a216e31a6e5e90096bb38c5
hvtnguyenson-code <hvtnguyenson@gmail.com>
2026-09-10 21:29:23 +0700

    feat(ppct): export component-aware allocation v2 service

--- apps/api/src/ppct-occurrence-allocation/ppct-occurrence-allocation.module.ts
+++ apps/api/src/ppct-occurrence-allocation/ppct-occurrence-allocation.module.ts
@@ -1,6 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ResolvedOccurrencesModule } from '../resolved-occurrences/resolved-occurrences.module';
+import { PpctOccurrenceAllocationV2Service } from './ppct-occurrence-allocation-v2.service';
 import { PpctOccurrenceAllocationService } from './ppct-occurrence-allocation.service';
 
-@Module({ imports: [ResolvedOccurrencesModule], providers: [PpctOccurrenceAllocationService], exports: [PpctOccurrenceAllocationService] })
+@Module({
+  imports: [ResolvedOccurrencesModule],
+  providers: [PpctOccurrenceAllocationService, PpctOccurrenceAllocationV2Service],
+  exports: [PpctOccurrenceAllocationService, PpctOccurrenceAllocationV2Service],
+})
 export class PpctOccurrenceAllocationModule {}
